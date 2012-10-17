6eabbb5565d5b529835cd36ac5d7d3c380416105
Jon-Michael Deldin <dev@jmdeldin.com>
2012-10-16 23:15:11 -0600

    Fix loading error of helpers.js

--- spec/buster.js
+++ spec/buster.js
@@ -1,12 +1,16 @@
+/*global module: true */
 var config = module.exports;
 
-config["specs"] = {
-  rootPath: "../",
-  environment: "browser",
-  sources: [
-    "lib/*.js"
-  ],
-  tests: [
-    "spec/*.spec.js"
-  ],
-}
+config.specs = {
+    rootPath: "../",
+    environment: "browser",
+    sources: [
+        // lib/helpers.js needs to be loaded first
+        "lib/helpers.js",
+        "lib/EnglishParser.js",
+        "lib/interpreters.js"
+    ],
+    specs: [
+        "spec/*.spec.js"
+    ]
+};
